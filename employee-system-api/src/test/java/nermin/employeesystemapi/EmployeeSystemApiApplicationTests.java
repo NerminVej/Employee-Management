ed7887399d6f01544666301b28acab6e48540543
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-21 18:26:16 +0100

    created the spring project

---
+++ employee-system-api/src/test/java/nermin/employeesystemapi/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nermin.employeesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
