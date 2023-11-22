158c6a9862c74769368b4a4dc59c7a92d3c6e6bc
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:47:54 +0100

    added the table column names

--- employee-management-system-ui/src/components/EmployeeList.tsx
+++ employee-management-system-ui/src/components/EmployeeList.tsx
@@ -8,7 +8,27 @@ export const EmployeeList = () => {
         Add Employee
       </button>
     </div>
-   
+    <div className="flex shadow border-b">
+      <table className="min-w-full">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+              First Name
+            </th>
+            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+              Last Name
+            </th>
+            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+              Email ID
+            </th>
+            <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+              Actions
+            </th>
+          </tr>
+        </thead>
+      
+      </table>
+    </div>
   </div>
   )
 }
