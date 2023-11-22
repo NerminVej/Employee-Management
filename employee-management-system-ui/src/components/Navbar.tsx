a09e1a85a5a634b8de0b3a0864c98f819d77e1eb
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:36:54 +0100

    created a simple navbar for this project can improve upon it later on

--- employee-management-system-ui/src/components/Navbar.tsx
+++ employee-management-system-ui/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center">
+        <p className="text-white font-bold">Employee Management System</p>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
