942b36bb951284759013e1ace7ba5bbf929e0152
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:45:19 +0100

    created the details of an employee

--- employee-management-system-ui/src/components/Employee.tsx
+++ employee-management-system-ui/src/components/Employee.tsx
@@ -1,6 +1,17 @@
 
 export const Employee = () => {
   return (
-    <div>Employee</div>
+    <tr >
+    <td className="text-left px-6 py-4 whitespace-nowrap">
+      <div className="text-sm text-gray-500">first name placeholder</div>
+    </td>
+    <td className="text-left px-6 py-4 whitespace-nowrap">
+      <div className="text-sm text-gray-500">last name placeholder</div>
+    </td>
+    <td className="text-left px-6 py-4 whitespace-nowrap">
+      <div className="text-sm text-gray-500">email placeholder</div>
+    </td>
+    
+  </tr>
   )
 }
