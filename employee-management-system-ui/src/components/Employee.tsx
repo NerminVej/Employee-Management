b58129a00734e8458ee5742bdbb119f4b954d303
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:45:32 +0100

    added the edit and delete button for now

--- employee-management-system-ui/src/components/Employee.tsx
+++ employee-management-system-ui/src/components/Employee.tsx
@@ -11,7 +11,16 @@ export const Employee = () => {
     <td className="text-left px-6 py-4 whitespace-nowrap">
       <div className="text-sm text-gray-500">email placeholder</div>
     </td>
-    
+    <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+      <a
+        className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">
+        Edit
+      </a>
+      <a
+        className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer">
+        Delete
+      </a>
+    </td>
   </tr>
   )
 }
