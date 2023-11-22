e18ae51e6eb4f9c94005917a830ad5d4d189b6ce
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:44:10 +0100

    created the Employee component on the frontend

--- employee-management-system-ui/src/App.tsx
+++ employee-management-system-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import { AddEmployee } from "./components/AddEmployee";
+import { Employee } from "./components/Employee";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Navbar />
       <AddEmployee />
+      <Employee />
     </div>
   );
 }
