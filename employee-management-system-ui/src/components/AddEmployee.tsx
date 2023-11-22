dc37338a3bded6a89f71023ef2e3323f89fab294
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:41:20 +0100

    implemented the page jsx structure of the add employee component

--- employee-management-system-ui/src/components/AddEmployee.tsx
+++ employee-management-system-ui/src/components/AddEmployee.tsx
@@ -14,7 +14,37 @@ export const AddEmployee = () => {
           <input
             type="text"
             name="firstName"
-            className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Email
+          </label>
+          <input
+            type="email"
+            name="emailId"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+
+        <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+          <button className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
+            Save
+          </button>
+          <button className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">
+            Clear
+          </button>
         </div>
       </div>
     </div>
