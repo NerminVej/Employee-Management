766ae9691faf86a0470538bafd976b42baad6c93
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-22 18:50:08 +0100

    created all the input fields for the Update form

--- employee-management-system-ui/src/components/UpdateEmployee.tsx
+++ employee-management-system-ui/src/components/UpdateEmployee.tsx
@@ -6,7 +6,33 @@ export const UpdateEmployee = () => {
       <div className="font-thin text-2xl tracking-wider">
         <h1>Update Employee</h1>
       </div>
-     
+      <div className="items-center justify-center h-14 w-full my-4">
+        <label className="block text-gray-600 text-sm font-normal">
+          First Name
+        </label>
+        <input
+          type="text"
+          name="firstName"
+          className="h-10 w-96 border mt-2 px-2 py-2"></input>
+      </div>
+      <div className="items-center justify-center h-14 w-full my-4">
+        <label className="block text-gray-600 text-sm font-normal">
+          Last Name
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          className="h-10 w-96 border mt-2 px-2 py-2"></input>
+      </div>
+      <div className="items-center justify-center h-14 w-full my-4">
+        <label className="block text-gray-600 text-sm font-normal">
+          Email
+        </label>
+        <input
+          type="email"
+          name="emailId"
+          className="h-10 w-96 border mt-2 px-2 py-2"></input>
+      </div>
 
       <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
         <button
