3390a3f31be0973cef45c408ddd9bca8df9fba5f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-23 18:59:27 +0100

    added the EmployeeService to the controller

--- employee-system-api/src/main/java/nermin/employeesystemapi/controller/EmployeeController.java
+++ employee-system-api/src/main/java/nermin/employeesystemapi/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package nermin.employeesystemapi.controller;
 
 
+import nermin.employeesystemapi.services.EmployeeService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 }
--- employee-system-api/src/main/java/nermin/employeesystemapi/services/EmployeeServiceImpl.java
+++ employee-system-api/src/main/java/nermin/employeesystemapi/services/EmployeeServiceImpl.java
@@ -1,4 +1,9 @@
 package nermin.employeesystemapi.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeServiceImpl {
+
+
 }
