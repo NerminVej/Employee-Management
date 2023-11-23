f86c84d721401aa5d4dabb1c11f218d5e3af727c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-23 19:05:31 +0100

    created all the methods I need for the CRUD operations

--- employee-system-api/src/main/java/nermin/employeesystemapi/services/EmployeeService.java
+++ employee-system-api/src/main/java/nermin/employeesystemapi/services/EmployeeService.java
@@ -1,4 +1,18 @@
 package nermin.employeesystemapi.services;
 
+import nermin.employeesystemapi.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
 }
