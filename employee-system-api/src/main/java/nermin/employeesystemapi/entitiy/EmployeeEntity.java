2e45ccbb1de85939249fd1bbd938c0c26cb26b1f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-23 19:00:22 +0100

    created the EmployeeEntity class

---
+++ employee-system-api/src/main/java/nermin/employeesystemapi/entitiy/EmployeeEntity.java
@@ -0,0 +1,13 @@
+package nermin.employeesystemapi.entitiy;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class EmployeeEntity {
+    
+}
