ad7c781d1a18fe7da66dc346716af2abdb5f76b3
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-23 19:01:08 +0100

    created the values of the Employee Entity

--- employee-system-api/src/main/java/nermin/employeesystemapi/entitiy/EmployeeEntity.java
+++ employee-system-api/src/main/java/nermin/employeesystemapi/entitiy/EmployeeEntity.java
@@ -2,12 +2,17 @@ package nermin.employeesystemapi.entitiy;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "employees")
 public class EmployeeEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
 }
