64240c066786c2af0afe75f353aae926644dbf54
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-27 18:29:53 +0100

    implemented the editEmployee function for the Employee

--- employee-management-system-ui/src/components/Employee.tsx
+++ employee-management-system-ui/src/components/Employee.tsx
@@ -1,35 +1,32 @@
 import { useState } from "react";
-
-
+import { useNavigate } from "react-router-dom";
 
 export const Employee = () => {
+  const navigate = useNavigate();
+  const editEmployee = (e, id) => {
+    e.preventDefault();
+    navigate(`/editEmployee/${id}`);
+  };
 
-  
   return (
-    <tr >
-    <td className="text-left px-6 py-4 whitespace-nowrap">
-      <div className="text-sm text-gray-500">first name placeholder</div>
-    </td>
-    <td className="text-left px-6 py-4 whitespace-nowrap">
-      <div className="text-sm text-gray-500">last name placeholder</div>
-    </td>
-    <td className="text-left px-6 py-4 whitespace-nowrap">
-      <div className="text-sm text-gray-500">email placeholder</div>
-    </td>
-    <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-      <a
-        className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">
-        Edit
-      </a>
-      <a
-        className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer">
-        Delete
-      </a>
-    </td>
-  </tr>
-  )
-}
-function useNavigate() {
-  throw new Error("Function not implemented.");
-}
-
+    <tr>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">first name placeholder</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">last name placeholder</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">email placeholder</div>
+      </td>
+      <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+        <a className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">
+          Edit
+        </a>
+        <a className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer">
+          Delete
+        </a>
+      </td>
+    </tr>
+  );
+};
