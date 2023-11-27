1c1bae17a845fdb2af15f801dddbcf058d136878
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-27 18:33:18 +0100

    imported useState

--- employee-management-system-ui/src/components/UpdateEmployee.tsx
+++ employee-management-system-ui/src/components/UpdateEmployee.tsx
@@ -1,8 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { EmployeeType } from "../types/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import EmployeeService from "../services/EmployeeService";
-
 
 export const UpdateEmployee = () => {
   const { id } = useParams();
@@ -45,49 +44,62 @@ export const UpdateEmployee = () => {
 
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b">
-    <div className="px-8 py-8">
-      <div className="font-thin text-2xl tracking-wider">
-        <h1>Update Employee</h1>
-      </div>
-      <div className="items-center justify-center h-14 w-full my-4">
-        <label className="block text-gray-600 text-sm font-normal">
-          First Name
-        </label>
-        <input
-          type="text"
-          name="firstName"
-          className="h-10 w-96 border mt-2 px-2 py-2"></input>
-      </div>
-      <div className="items-center justify-center h-14 w-full my-4">
-        <label className="block text-gray-600 text-sm font-normal">
-          Last Name
-        </label>
-        <input
-          type="text"
-          name="lastName"
-          className="h-10 w-96 border mt-2 px-2 py-2"></input>
-      </div>
-      <div className="items-center justify-center h-14 w-full my-4">
-        <label className="block text-gray-600 text-sm font-normal">
-          Email
-        </label>
-        <input
-          type="email"
-          name="emailId"
-          className="h-10 w-96 border mt-2 px-2 py-2"></input>
-      </div>
+      <div className="px-8 py-8">
+        <div className="font-thin text-2xl tracking-wider">
+          <h1>Update Employee</h1>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="firstName"
+            value={employee.firstName}
+            onChange={(e) => handleChange(e)}
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            value={employee.lastName}
+            onChange={(e) => handleChange(e)}
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Email
+          </label>
+          <input
+            type="email"
+            name="emailId"
+            value={employee.emailId}
+            onChange={(e) => handleChange(e)}
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
 
-      <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-        <button
-          className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
-          Update
-        </button>
-        <button
-          className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">
-          Cancel
-        </button>
+        <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+          <button
+            onClick={updateEmployee}
+            className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6"
+          >
+            Update
+          </button>
+          <button
+            onClick={() => navigate("/employeeList")}
+            className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
