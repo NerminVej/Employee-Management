1351d7d3c144d1ca21cb857c7d44786dbe54e2e0
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-29 18:20:33 +0100

    added some types to the service methods

--- employee-management-system-ui/src/services/EmployeeService.ts
+++ employee-management-system-ui/src/services/EmployeeService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { EmployeeType } from "../types/types";
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
@@ -17,7 +18,7 @@ class EmployeeService {
     return axios.get(EMPLOYEE_API_BASE_URL + "/" + id);
   }
 
-  updateEmployee(employee, id) {
+  updateEmployee(employee: EmployeeType, id: string | undefined) {
     return axios.put(EMPLOYEE_API_BASE_URL + "/" + id, employee);
   }
 }
