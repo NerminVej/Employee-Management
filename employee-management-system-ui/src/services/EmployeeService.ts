6524c31010aa61cec3c57bb90a64307da8c8e874
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-30 19:59:10 +0100

    did the same for the employee

--- employee-management-system-ui/src/services/EmployeeService.ts
+++ employee-management-system-ui/src/services/EmployeeService.ts
@@ -4,14 +4,14 @@ import { EmployeeType } from "../types/types";
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 class EmployeeService {
-  saveEmployee(employee) {
+  saveEmployee(employee: EmployeeType) {
     return axios.post(EMPLOYEE_API_BASE_URL, employee);
   }
   getEmployees() {
     return axios.get(EMPLOYEE_API_BASE_URL);
   }
 
-  deleteEmployee(id) {
+  deleteEmployee(id: string) {
     return axios.delete(EMPLOYEE_API_BASE_URL + "/" + id);
   }
   getEmployeeById(id: string) {
