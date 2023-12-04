a00cccf78adb272ebc3d94ab5cb3fe5e978cedd4
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-04 18:12:04 +0100

    imported all the packages and added the JPARepository to the UserRepository

--- employee-system-api/src/main/java/nermin/employeesystemapi/repository/UserRepository.java
+++ employee-system-api/src/main/java/nermin/employeesystemapi/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package nermin.employeesystemapi.repository;
 
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
